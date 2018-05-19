6cdfb189d003989edf7fb947de61ed3f697d7ed5
morgan <mwcmitchell@gmail.com>
2018-05-19 15:52:12 -0400

    more

--- w3whaiku.py
+++ w3whaiku.py
@@ -3,11 +3,25 @@ import math
 from os import environ
 
 import what3words
+from pyphen import Pyphen
 from glom import glom
 
-def main():
+
+def setup():
     key = environ['w3wkey']
     w3w = what3words.Geocoder(key)
+    dic = Pyphen(lang='en')
+    return w3w, dic
+
+
+def count_syllables(dictionary, word):
+    count = 0
+    hyphenated = dictionary.inserted(word)
+    count = hyphenated.count('-') + 1
+    return hyphenated, count
+
+
+def main():
 
     long_max = 180
     long_min = -180
@@ -15,11 +29,18 @@ def main():
     lat_max = 85.05112878
     lat_min = -85.05112878
 
+    w3w, dic = setup()
+
     random_lat = random.uniform(lat_min, lat_max)
     random_long = random.uniform(long_min, long_max)
 
     res = w3w.reverse(lng=random_long, lat=random_lat)
-    print(' '.join(glom(res, 'words').split('.')))
+
+    words = glom(res, 'words').split('.')
+
+    for word in words:
+        count, hyphenated = count_syllables(dic, word)
+        print(word, count, hyphenated)
 
     raise SystemExit()
 
